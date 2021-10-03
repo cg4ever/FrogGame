fe7c9db3c1e63050ede131d4220776e5406378d8
cg4ever <christian.goeth@yahoo.de>
2021-10-03 16:56:42 +0200

    show-functions updated

--- simulations/show_results_collisions.py
+++ simulations/show_results_collisions.py
@@ -18,12 +18,13 @@ for key,values in dic.items():
     print(key, 'number of wins = ', sum(1 for i in values if i < 3))
     runs = len(values)
 
+del dic[(50,50)], dic[(50,100)], dic[(50,125)], dic[(75,125)], dic[(100,50)], dic[(100,75)], dic[(125,50)], dic[(125,75)], dic[(125,100)], dic[(75,50)], dic[(50,75)]
 
 fig, ax = plt.subplots()
 ax.set_title('Boxplot of simulation results with ' + str(runs) + ' runs')
 ax.boxplot(dic.values(), patch_artist=True)
 ax.set_xticklabels(dic.keys())
 ax.yaxis.grid(True)
-ax.set_xlabel('Different periods (pf, ps)')
-ax.set_ylabel('Number of wins')
+ax.set_xlabel('Different periods $(p_F, p_S)$')
+ax.set_ylabel('Number of collisions until win')
 plt.show()
